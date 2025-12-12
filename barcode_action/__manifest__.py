28e6a47980608a52a87a788966de4add81f5ae76
dimaurosalvatore <i.dimauro@pixora.it>
2025-12-12 16:03:29 +0100

    [MIG] manifest to 19.0

--- barcode_action/__manifest__.py
+++ barcode_action/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Barcode action launcher",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Extra Tools",
     "website": "https://github.com/OCA/server-ux",
     "author": "Creu Blanca, ForgeFlow, Odoo Community Association (OCA)",
